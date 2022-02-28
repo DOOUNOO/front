d42e86bb6bf4990cc675cd71ee806391aa32907e
Josh <joshua.m.rivas@gmail.com>
2022-02-28 19:16:31 +0100

    WIP Header and Subheader; Added token state in App.js; Added Inter  font

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Home from "./containers/Home";
-import Signup from "./containers/SignUp"
-import Login from "./containers/Login"
-import Publish from "./containers/Publish"
-import Offer from "./containers/Offer"
-import Payment from "./containers/Payment"
-import PageNotFound from "./containers/PageNotFound"
+import Signup from "./containers/SignUp";
+import Login from "./containers/Login";
+import Publish from "./containers/Publish";
+import Offer from "./containers/Offer";
+import Payment from "./containers/Payment";
+import PageNotFound from "./containers/PageNotFound";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <Router>
-      <Header />
+      <Header token={token} setToken={setToken} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,62 @@
-import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ token, setToken }) => {
   return (
     <div className="header-container">
+      <div className="header-left">
+        <div className="logo-div">DOOUNOO</div>
+      </div>
+
+      {token ? (
+        <div className="header-right logged-in">
+          <div className="menu-item-container find-a-service">
+            <div className="find-a-service">Trouver un service</div>
+            <div className="underline"></div>
+          </div>
+          <Link
+            to="/publish"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="menu-item-container offer-a-service">
+              <div className="offer-a-service">Proposer un service</div>
+              <div className="underline"></div>
+            </div>
+          </Link>
+          <div className="menu-item-container">Messages</div>
+          <div className="menu-item-container">Mes favoris</div>
+          <div className="menu-item-container">Avatar</div>
+        </div>
+      ) : (
+        <div className="header-right logged-out">
+          <div className="menu-item-container find-a-service">
+            <div className="find-a-service">Trouver un service</div>
+            <div className="underline"></div>
+          </div>
+
+          <Link
+            to="/publish"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="menu-item-container offer-a-service">
+              <div className="offer-a-service">Proposer un service</div>
+              <div className="underline"></div>
+            </div>
+          </Link>
+          <Link
+            to="/login"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="menu-item-container">Se connecter</div>
+          </Link>
+          <Link
+            to="/signup"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="menu-item-container sign-up-btn">S'inscrire</div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
