f0e60c8e6150e7c8a0c03701411115859678f55f
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-03-01 16:20:56 +0100

    adding button component

---
+++ src/components/Button.js/index.js
@@ -0,0 +1,32 @@
+import "./style.scss";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+
+const Button = ({ rounded, noBackground, squared, children, linkUrl }) => {
+  return (
+    /*  This button has conditional display options depending on where it is displayed
+  
+Conditional display : 
+-rounded (with turquoise background)
+-squared (with turquoise background)
+-noBackground (without background)
+
+*/
+
+    /* This button appear on : DoubleButtonsRate component */
+
+    <Link to={linkUrl}>
+      <button
+        className={classNames("cta-btn", {
+          squared: squared,
+          rounded: rounded,
+          noBackground: noBackground,
+        })}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div className="Home-container">
-    </div>
-  );
+  return <div className="Home-container"></div>;
 };
 
 export default Home;
