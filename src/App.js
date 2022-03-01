1458ef38393630ed0c745eddbb24c70f3bcfa934
Josh <joshua.m.rivas@gmail.com>
2022-03-01 15:42:20 +0100

    resolved warning message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
