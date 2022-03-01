a819e01d59a55496cd11c8959d78e267a61ff2d8
elarchi <emilie.leury@gmail.com>
2022-03-01 23:53:25 +0100

    add <StudentSignupForm/> && /signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 import Header from "./components/Header/Header";
 import Home from "./containers/Home";
 import Signup from "./containers/SignUp";
@@ -11,15 +12,27 @@ import Payment from "./containers/Payment";
 import PageNotFound from "./containers/PageNotFound";
 
 function App() {
-  const [token, setToken] = useState(null);
-  console.log(token);
+  const [token, setToken] = useState(Cookies.get("userToken") || null);
+
+  const setUser = (token, id) => {
+    if (token) {
+      // Gestion de cookie
+      Cookies.set("userToken", token, { expires: 10 });
+      Cookies.set("userId", id, { expires: 10 });
+    } else {
+      Cookies.remove("userToken");
+      Cookies.remove("userId");
+    }
+
+    setToken(token);
+  };
 
   return (
     <Router>
       <Header token={token} setToken={setToken} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup setUser={setUser} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/publish" element={<Publish />} />
         <Route path="/offer/:id" element={<Offer />} />
--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import StudentSignupForm from "../../components/StudentSignupForm/StudentSignupForm";
 
 import "./index.css";
 
 const Signup = () => {
+  const [userType, setUserType] = useState(0);
+  // state 0 : no information about the user yet
+  // state 1 : the user is a Student
+  // state 2 : the user is a Business
+
   return (
     <div className="signup-container">
+      {userType === 0 ? (
+        <div>
+          <h1>Êtes-vous ...</h1>
+          <button
+            onClick={() => {
+              setUserType(1);
+            }}
+          >
+            Un.e étudiant.e
+          </button>
+          <button
+            onClick={() => {
+              setUserType(2);
+            }}
+          >
+            Une entreprise
+          </button>
+        </div>
+      ) : (
+        <div>
+          {userType === 1 ? <StudentSignupForm /> : <span>is a business</span>}
+        </div>
+      )}
     </div>
   );
 };
