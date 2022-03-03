6ca4dd9321baa79abb0f5474976edd2d8f3c2f73
elarchi <emilie.leury@gmail.com>
2022-03-03 18:26:28 +0100

    scss expert signup

--- src/containers/ExpertSignUp/index.js
+++ src/containers/ExpertSignUp/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+// import smillingBoy from "/Users/emilieleury/08-leReacteur/6-doounoo/doounoo-front/src/assets/images/smiling_boy.jpg";
 
 import "./index.scss";
 
@@ -11,7 +12,8 @@ const ExpertSignup = ({ setUser }) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
-  const [amIARobot, setAmIARobot] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
+  const [isRobot, setIsRobot] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -20,18 +22,24 @@ const ExpertSignup = ({ setUser }) => {
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
-
-      const response = await axios.post(
-        "https://doounoo.herokuapp.com/expert/signup",
-        {
-          email,
-          password,
+      if (password === confirmPassword) {
+        const response = await axios.post(
+          "https://doounoo.herokuapp.com/expert/signup",
+          {
+            email,
+            password,
+            account: {
+              firstName,
+              lastName,
+            },
+          }
+        );
+        if (response.data.token) {
+          setUser(response.data.token, response.data._id);
+          navigate("/");
         }
-      );
-
-      if (response.data.token) {
-        setUser(response.data.token, response.data._id);
-        navigate("/");
+      } else {
+        setErrorMessage("Vos mot-de-passes ne concordent pas");
       }
     } catch (error) {
       console.log("ExpertSignup Error ===>", error.message);
@@ -44,61 +52,75 @@ const ExpertSignup = ({ setUser }) => {
   };
 
   return (
-    <div>
-      <h1>Inscrivez-vous</h1>
-      <span>
-        Vous avez déjà un compte ? <span>Connectez-vous</span>{" "}
-      </span>
-      <form onSubmit={handleSubmit}>
-        <h2>Votre prénom</h2>
-        <input
-          type="text"
-          placeholder=""
-          onChange={(event) => {
-            setFirstName(event.target.value);
-          }}
-        />
+    <div className="expert-signup__div">
+      <div className="expert-signup_framed__div">
+        <h1>Monétisez vos compétences en les partageant</h1>
+        <h6>Faites appel à votre expérience et travailler à votre rythme</h6>
+        <form onSubmit={handleSubmit} className="expert-signup__form">
+          <div className="expert-signup_form_names__div">
+            <input
+              type="text"
+              placeholder="Nom"
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Prénom"
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            />
+          </div>
 
-        <h2>Votre nom</h2>
-        <input
-          type="text"
-          placeholder=""
-          onChange={(event) => {
-            setLastName(event.target.value);
-          }}
-        />
-        <h2>Votre e-mail</h2>
-        <input
-          type="email"
-          placeholder=""
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <h2>Créez un mot de passe</h2>
-        <input
-          type="password"
-          placeholder=""
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <h2>Confirmez votre mot de passe</h2>
-        <input
-          type="password"
-          placeholder=""
-          onChange={(event) => {
-            setConfirmPassword(event.target.value);
-          }}
-        />
-        <label>
-          <input type="checkbox" />
-          Se souvenir de moi
-        </label>
-        <span>CAPTCHA je ne suis pas un robot</span>
-        <input type="submit" value="Valider l'inscription" />
-        <span>{errorMessage}</span>
-      </form>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+
+          <input
+            type="password"
+            placeholder="Confirmer le mot de passe"
+            onChange={(event) => {
+              setConfirmPassword(event.target.value);
+            }}
+          />
+          <label>
+            <input type="checkbox" />
+            Se souvenir de moi
+          </label>
+          <label>
+            <input type="checkbox" />
+            Je ne suis pas un robot
+          </label>
+          <input
+            type="submit"
+            value="Valider l'inscription"
+            className="expert-signup_form_submit__input"
+          />
+          <span>{errorMessage}</span>
+        </form>
+        <p>
+          En vous inscrivant, vous acceptez les
+          <span className="expert-signup_blue-span__span">
+            {" "}
+            Conditions Générales d'Utilisation{" "}
+          </span>
+          et <span> la Politique de Confidentialité de Doounoo</span>
+        </p>
+      </div>
     </div>
   );
 };
