49b2f850c1e29294d52a07cd8e46fe3103b4422b
Alexandre  Roux <aroux@softbankrobotics.com>
2022-03-04 10:58:04 +0100

    Add a nice visual effect on buttons in Sign Up page

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./index.scss";
@@ -6,33 +6,28 @@ import "./index.scss";
 const Signup = () => {
   const navigate = useNavigate();
 
-  const [userType, setUserType] = useState(0);
-  // state 0 : no information about the user yet
-  // state 1 : the user is a Student
-  // state 2 : the user is a Business
-
   return (
     <div className="signup-container signup__div">
       <div className="signup_framed__div">
         <h1>Je souhaite ...</h1>
         <div className="signup_framed_buttons__div">
-          <button
-            className="user__button"
+          <div
+            className="button user__button"
             onClick={() => {
               navigate("/signup/user");
             }}
           >
-            Trouver un.e expert.e
-          </button>
+            <p>Trouver un.e expert.e</p>
+          </div>
 
-          <button
-            className="expert__button"
+          <div
+            className="button expert__button"
             onClick={() => {
               navigate("/signup/expert");
             }}
           >
-            Partager et monétiser mon expertise
-          </button>
+            <p>Partager et monétiser mon expertise</p>
+          </div>
         </div>
       </div>
     </div>
