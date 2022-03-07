80918f18c5f25d31e1f71e1a4c66688b34580af1
Josh <joshua.m.rivas@gmail.com>
2022-03-07 17:25:47 +0100

    Created CheckoutForm component

--- src/containers/Payment/index.js
+++ src/containers/Payment/index.js
@@ -1,5 +1,7 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../../components/CheckoutForm/index";
 
 import "./index.css";
 
