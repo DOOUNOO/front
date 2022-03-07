d293af11c4280aeae7f114eef4833bf65fa1173b
Josh <joshua.m.rivas@gmail.com>
2022-03-07 17:37:43 +0100

    Reinstalled Calendar package

--- src/containers/Payment/index.js
+++ src/containers/Payment/index.js
@@ -2,13 +2,13 @@ import { useLocation } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../../components/CheckoutForm/index";
-
 import "./index.css";
+
+const stripePromise = loadStripe("pk_test_5z9rSB8XwuAOihoBixCMfL6X");
 
 const Payment = () => {
   const { state } = useLocation();
-  const userData = state.userData;
-  const expertData = state.expertData;
+  const { userData, expertData } = state;
 
   return (
     <div className="payment-container">
